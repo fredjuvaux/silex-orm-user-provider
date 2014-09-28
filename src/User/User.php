232d90f945d5b85bd0aae0fb4b44e1282153334c
fredjuvaux <Fred@MBP-Fred.local>
2014-09-28 18:27:36 +0200

    Creation

---
+++ src/User/User.php
@@ -0,0 +1,322 @@
+<?php
+
+namespace Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * User
+ *
+ * @Entity 
+ * @Table(name="user")
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @var integer
+     *
+     * @Column(name="id", type="integer")
+     * @Id
+     * @GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @Column(name="salt", type="string", length=255)
+     */
+    private $salt;
+
+    /**
+     * @var array
+     *
+     * @Column(name="roles", type="array")
+     */
+    private $roles = array();
+
+    /**
+     * @var integer
+     *
+     * @Column(name="created", type="integer")
+     */
+    private $created;
+
+    /**
+     * Constructor.
+     *
+     * @param string $email
+     */
+    public function __construct($email)
+    {
+        $this->email = $email;
+        $this->created = time();
+        $this->salt = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
+    }
+
+    /**
+     * Returns the roles granted to the user. Note that all users have the ROLE_USER role.
+     *
+     * @return array A list of the user's roles.
+     */
+    public function getRoles()
+    {
+        $roles = $this->roles;
+
+        // Every user must have at least one role, per Silex security docs.
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * Set the user's roles to the given list.
+     *
+     * @param array $roles
+     */
+    public function setRoles(array $roles)
+    {
+        $this->roles = array();
+
+        foreach ($roles as $role) {
+            $this->addRole($role);
+        }
+    }
+
+    /**
+     * Test whether the user has the given role.
+     *
+     * @param string $role
+     * @return bool
+     */
+    public function hasRole($role)
+    {
+        return in_array(strtoupper($role), $this->getRoles(), true);
+    }
+
+    /**
+     * Add the given role to the user.
+     *
+     * @param string $role
+     */
+    public function addRole($role)
+    {
+        $role = strtoupper($role);
+
+        if ($role === 'ROLE_USER') {
+            return;
+        }
+
+        if (!$this->hasRole($role)) {
+            $this->roles[] = $role;
+        }
+    }
+
+    /**
+     * Remove the given role from the user.
+     *
+     * @param string $role
+     */
+    public function removeRole($role)
+    {
+        if (false !== $key = array_search(strtoupper($role), $this->roles, true)) {
+            unset($this->roles[$key]);
+            $this->roles = array_values($this->roles);
+        }
+    }
+
+    /**
+     * Set the user ID.
+     *
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Get the user ID.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the encoded password used to authenticate the user.
+     *
+     * On authentication, a plain-text password will be salted,
+     * encoded, and then compared to this value.
+     *
+     * @return string The encoded password.
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the encoded password.
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Set the salt that should be used to encode the password.
+     *
+     * @param string $salt
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string The salt
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Returns the email address, which serves as the username used to authenticate the user.
+     *
+     * This method is required by the UserInterface.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string The user's email address.
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Set the time the user was originally created.
+     *
+     * @param int $created A timestamp value.
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * Set the time the user was originally created.
+     *
+     * @return int
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is a no-op, since we never store the plain text credentials in this object.
+     * It's required by UserInterface.
+     *
+     * @return void
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * The Symfony Security component stores a serialized User object in the session.
+     * We only need it to store the user ID, because the user provider's refreshUser() method is called on each request
+     * and reloads the user by its ID.
+     *
+     * @see \Serializable::serialize()
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+        ));
+    }
+
+    /**
+     * @see \Serializable::unserialize()
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            ) = unserialize($serialized);
+    }
+
+    /**
+     * Validate the user object.
+     *
+     * @return array An array of error messages, or an ampty array if there were no errors.
+     */
+    public function validate()
+    {
+        $errors = array();
+
+        if (!$this->getEmail()) {
+            $errors['email'] = 'Email address is required.';
+        } else if (!strpos($this->getEmail(), '@')) {
+            // Basic email format sanity check. Real validation comes from sending them an email with a link they have to click.
+            $errors['email'] = 'Email address appears to be invalid.';
+        } else if (strlen($this->getEmail()) > 100) {
+            $errors['email'] = 'Email address can\'t be longer than 100 characters.';
+        }
+
+        if (!$this->getPassword()) {
+            $errors['password'] = 'Password is required.';
+        } else if (strlen($this->getPassword()) > 255) {
+            $errors['password'] = 'Password can\'t be longer than 255 characters.';
+        }
+
+        if (strlen($this->getName()) > 100) {
+            $errors['name'] = 'Name can\'t be longer than 100 characters.';
+        }
+
+        return $errors;
+    }
+}
